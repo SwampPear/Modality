2fe5abf30025a61164d46676315b05658f97c51e
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-02-01 14:18:07 -0500

    setup basic structure for modals and containers

---
+++ modality/src/components/Modal/Modal.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Modal.css'
+
+const Modal = () => {
+  return (
+    <div className='modality__container'>
+      <div className='modality__modal'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Modal
