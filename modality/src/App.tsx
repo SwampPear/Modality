87e26421f702aa56594c23f38e1fb59280d2c9c4
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-02-01 14:25:28 -0500

    project is fixed

--- modality/src/App.tsx
+++ modality/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 
+import Modal from './components/Modal/Modal'
+
 const App = () => {
   return (
     <div className="App">
-      
+      <Modal/>
     </div>
   )
 }
