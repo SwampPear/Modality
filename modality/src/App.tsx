ade19a8a2f47eb37f443b2b03a765664bad2104f
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-02-01 22:09:37 -0500

    set up props on modal component to use optional css classes

--- modality/src/App.tsx
+++ modality/src/App.tsx
@@ -6,7 +6,7 @@ import Modal from './components/Modal/Modal'
 const App = () => {
   return (
     <div className="App">
-      <Modal/>
+      <Modal>This is a test sentence</Modal>
     </div>
   )
 }
--- modality/src/components/Modal/Modal.tsx
+++ modality/src/components/Modal/Modal.tsx
@@ -1,14 +1,55 @@
 import React from 'react'
 import './Modal.css'
 
-const Modal = () => {
+
+interface IModalRequiredProps {
+  children: (string | JSX.Element)[] | string | JSX.Element
+}
+
+interface IModalOptionalProps {
+  containerClass: string
+  backgroundClass: string
+  modalClass: string
+}
+
+interface IModalProps
+  extends IModalRequiredProps,
+    IModalOptionalProps {}
+
+
+const defaultProps: IModalOptionalProps = {
+  containerClass: '',
+  backgroundClass: '',
+  modalClass: ''
+}
+
+const Modal = (props: IModalProps) => {
   return (
-    <div className='modality__container'>
-      <div className='modality__modal'>
-        
+    <>
+      <div 
+        className={
+          `modality__background 
+          ${props.backgroundClass ? props.backgroundClass : ''}`
+        }
+      />
+      <div 
+        className={
+          `modality__container 
+          ${props.containerClass ? props.containerClass : ''}`
+        }
+      >
+        <div 
+          className={
+            `modality__modal
+            ${props.modalClass ? props.modalClass : ''}`
+          }
+        >
+          {props.children}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
+Modal.defaultProps = defaultProps
 export default Modal
